39d0e2eda807b9d79e231fb7ebb8584dd07c35e2
Ken Youens-Clark <kyclark@gmail.com>
2019-11-22 08:06:22 -0700

    added flags

--- run_pear/src/lib.rs
+++ run_pear/src/lib.rs
@@ -17,6 +17,23 @@ use std::{
 pub struct Config {
     query: Vec<String>,
     out_dir: PathBuf,
+    p_value: Option<f32>,
+    min_overlap: Option<u32>,
+    max_assembly_length: Option<u32>,
+    min_assembly_length: Option<u32>,
+    min_trim_length: Option<u32>,
+    quality_threshold: Option<u32>,
+    max_uncalled_base: Option<f32>,
+    test_method: Option<u32>,
+    empirical_freqs: Option<bool>,
+    score_method: Option<u32>,
+    phred_base: Option<u32>,
+    memory: Option<String>,
+    cap: Option<u32>,
+    threads: Option<u32>,
+    nbase: Option<bool>,
+    keep_original: Option<bool>,
+    stitch: Option<bool>,
     num_concurrent_jobs: Option<u32>,
     num_halt: Option<u32>,
 }
@@ -39,7 +56,7 @@ pub fn get_args() -> MyResult<Config> {
         .about("Runs Pear")
         .arg(
             Arg::with_name("query")
-                .short("q")
+                .short("Q")
                 .long("query")
                 .value_name("FILE_OR_DIR")
                 .help("File input or directory")
@@ -54,8 +71,123 @@ pub fn get_args() -> MyResult<Config> {
                 .help("Output directory"),
         )
         .arg(
-            Arg::with_name("num_concurrent_jobs")
+            Arg::with_name("p_value")
+                .short("p")
+                .long("p_value")
+                .value_name("FLOAT")
+                .help("P-value"),
+        )
+        .arg(
+            Arg::with_name("min_overlap")
+                .short("v")
+                .long("min_overlap")
+                .value_name("INT")
+                .help("Minimum overlap"),
+        )
+        .arg(
+            Arg::with_name("max_assembly_length")
+                .short("m")
+                .long("max_assembly_length")
+                .value_name("INT")
+                .help("Max assembly length"),
+        )
+        .arg(
+            Arg::with_name("min_assembly_length")
                 .short("n")
+                .long("min_assembly_length")
+                .value_name("INT")
+                .help("Max assembly length"),
+        )
+        .arg(
+            Arg::with_name("min_trim_length")
+                .short("t")
+                .long("min_trim_length")
+                .value_name("INT")
+                .help("Minimum assembly length"),
+        )
+        .arg(
+            Arg::with_name("quality_threshold")
+                .short("q")
+                .long("quality_threshold")
+                .value_name("INT")
+                .help("Quality threshold"),
+        )
+        .arg(
+            Arg::with_name("max_uncalled_base")
+                .short("u")
+                .long("max_uncalled_base")
+                .value_name("FLOAT")
+                .help("Max proportion of uncalled bases"),
+        )
+        .arg(
+            Arg::with_name("test_method")
+                .short("g")
+                .long("test_method")
+                .value_name("INT")
+                .help("Type  of  statistical  test"),
+        )
+        .arg(
+            Arg::with_name("empirical_freqs")
+                .short("e")
+                .long("empirical_freqs")
+                .help("Disable  empirical base frequencies"),
+        )
+        .arg(
+            Arg::with_name("score_method")
+                .short("s")
+                .long("score_method")
+                .value_name("INT")
+                .help("Scoring method"),
+        )
+        .arg(
+            Arg::with_name("phred_base")
+                .short("b")
+                .long("phred_base")
+                .value_name("INT")
+                .help("Base PHRED quality score"),
+        )
+        .arg(
+            Arg::with_name("memory")
+                .short("y")
+                .long("memory")
+                .value_name("STR")
+                .help("Amount of memory to be used"),
+        )
+        .arg(
+            Arg::with_name("cap")
+                .short("c")
+                .long("cap")
+                .value_name("INT")
+                .help("Upper bound for the resulting quality score"),
+        )
+        .arg(
+            Arg::with_name("threads")
+                .short("j")
+                .long("threads")
+                .value_name("INT")
+                .help("Number of threads to use"),
+        )
+        .arg(
+            Arg::with_name("nbase")
+                .short("z")
+                .long("nbase")
+                .help("Nbase"),
+        )
+        .arg(
+            Arg::with_name("keep_original")
+                .short("k")
+                .long("keep_original")
+                .help("Keep original"),
+        )
+        .arg(
+            Arg::with_name("stitch")
+                .short("i")
+                .long("stitch")
+                .help("concatenate reads"),
+        )
+        .arg(
+            Arg::with_name("num_concurrent_jobs")
+                .short("J")
                 .long("num_concurrent_jobs")
                 .value_name("INT")
                 .default_value("8")
@@ -79,6 +211,68 @@ pub fn get_args() -> MyResult<Config> {
         }
     };
 
+    let p_value = matches
+        .value_of("p_value")
+        .and_then(|x| x.trim().parse::<f32>().ok());
+
+    let min_overlap = matches
+        .value_of("min_overlap")
+        .and_then(|x| x.trim().parse::<u32>().ok());
+
+    let max_assembly_length = matches
+        .value_of("max_assembly_length")
+        .and_then(|x| x.trim().parse::<u32>().ok());
+
+    let min_assembly_length = matches
+        .value_of("min_assembly_length")
+        .and_then(|x| x.trim().parse::<u32>().ok());
+
+    let min_trim_length = matches
+        .value_of("min_trim_length")
+        .and_then(|x| x.trim().parse::<u32>().ok());
+
+    let quality_threshold = matches
+        .value_of("quality_threshold")
+        .and_then(|x| x.trim().parse::<u32>().ok());
+
+    let max_uncalled_base = matches
+        .value_of("max_uncalled_base")
+        .and_then(|x| x.trim().parse::<f32>().ok());
+
+    let test_method = matches
+        .value_of("test_method")
+        .and_then(|x| x.trim().parse::<u32>().ok());
+
+    let empirical_freqs = Some(matches.is_present("empirical_freqs"));
+
+    let score_method = matches
+        .value_of("score_method")
+        .and_then(|x| x.trim().parse::<u32>().ok());
+
+    let phred_base = matches
+        .value_of("phred_base")
+        .and_then(|x| x.trim().parse::<u32>().ok());
+
+    let memory = if let Some(memory) = matches.value_of("memory") {
+        Some(memory.to_string())
+    } else {
+        None
+    };
+
+    let cap = matches
+        .value_of("cap")
+        .and_then(|x| x.trim().parse::<u32>().ok());
+
+    let threads = matches
+        .value_of("threads")
+        .and_then(|x| x.trim().parse::<u32>().ok());
+
+    let nbase = Some(matches.is_present("nbase"));
+
+    let keep_original = Some(matches.is_present("keep_original"));
+
+    let stitch = Some(matches.is_present("stitch"));
+
     let num_concurrent_jobs = matches
         .value_of("num_concurrent_jobs")
         .and_then(|x| x.trim().parse::<u32>().ok());
@@ -92,6 +286,23 @@ pub fn get_args() -> MyResult<Config> {
         out_dir,
         num_concurrent_jobs,
         num_halt,
+        p_value,
+        min_overlap,
+        max_assembly_length,
+        min_assembly_length,
+        min_trim_length,
+        quality_threshold,
+        max_uncalled_base,
+        test_method,
+        empirical_freqs,
+        score_method,
+        phred_base,
+        memory,
+        cap,
+        threads,
+        nbase,
+        keep_original,
+        stitch,
     })
 }
 
@@ -134,6 +345,83 @@ fn make_jobs(
     config: &Config,
     pairs: ReadPairLookup,
 ) -> Result<Vec<String>, Box<dyn Error>> {
+    let mut args: Vec<String> = vec![];
+    if let Some(p_value) = config.p_value {
+        args.push(format!("-p {}", p_value));
+    }
+
+    if let Some(min_overlap) = config.min_overlap {
+        args.push(format!("-v {}", min_overlap));
+    }
+
+    if let Some(max_assembly_length) = config.max_assembly_length {
+        args.push(format!("-m {}", max_assembly_length));
+    }
+
+    if let Some(min_assembly_length) = config.min_assembly_length {
+        args.push(format!("-n {}", min_assembly_length));
+    }
+
+    if let Some(min_trim_length) = config.min_trim_length {
+        args.push(format!("-t {}", min_trim_length));
+    }
+
+    if let Some(quality_threshold) = config.quality_threshold {
+        args.push(format!("-q {}", quality_threshold));
+    }
+
+    if let Some(max_uncalled_base) = config.max_uncalled_base {
+        args.push(format!("-u {}", max_uncalled_base));
+    }
+
+    if let Some(test_method) = config.test_method {
+        args.push(format!("-g {}", test_method));
+    }
+
+    if let Some(empirical_freqs) = config.empirical_freqs {
+        if empirical_freqs {
+            args.push("-e".to_string());
+        }
+    }
+
+    if let Some(score_method) = config.score_method {
+        args.push(format!("-s {}", score_method));
+    }
+
+    if let Some(phred_base) = config.phred_base {
+        args.push(format!("-b {}", phred_base));
+    }
+
+    if let Some(memory) = &config.memory {
+        args.push(format!("-y {}", memory));
+    }
+
+    if let Some(cap) = config.cap {
+        args.push(format!("-c {}", cap));
+    }
+
+    if let Some(threads) = config.threads {
+        args.push(format!("-j {}", threads));
+    }
+
+    if let Some(nbase) = config.nbase {
+        if nbase {
+            args.push("-z".to_string());
+        }
+    }
+
+    if let Some(keep_original) = config.keep_original {
+        if keep_original {
+            args.push("-k".to_string());
+        }
+    }
+
+    if let Some(stitch) = config.stitch {
+        if stitch {
+            args.push("-i".to_string());
+        }
+    }
+
     let mut jobs: Vec<String> = vec![];
     for (i, (sample, val)) in pairs.iter().enumerate() {
         println!("{:3}: {}", i + 1, sample);
@@ -144,10 +432,11 @@ fn make_jobs(
         ) {
             let out_file = &config.out_dir.join(sample);
             jobs.push(format!(
-                "pear -f {} -r {} -o {}",
+                "pear -f {} -r {} -o {} {}",
                 fwd,
                 rev,
-                out_file.display()
+                out_file.display(),
+                args.join(" "),
             ));
         }
     }
@@ -182,13 +471,9 @@ fn find_files(paths: &[String]) -> Result<Vec<String>, Box<dyn Error>> {
 
 // --------------------------------------------------
 fn classify(paths: &[String]) -> Result<ReadPairLookup, Box<dyn Error>> {
-    let paths = paths.into_iter().map(Path::new);
-    let mut exts: Vec<String> = paths
-        .clone()
-        .into_iter()
-        .map(get_extension)
-        .filter_map(|x| x)
-        .collect();
+    let paths = paths.iter().map(Path::new);
+    let mut exts: Vec<String> =
+        paths.clone().map(get_extension).filter_map(|x| x).collect();
     exts.dedup();
 
     let dots = Regex::new(r"\.").unwrap();
@@ -201,7 +486,7 @@ fn classify(paths: &[String]) -> Result<ReadPairLookup, Box<dyn Error>> {
     let re = Regex::new(&pattern).unwrap();
 
     let mut reads: ReadPairLookup = HashMap::new();
-    for path in paths.into_iter().map(Path::new) {
+    for path in paths.map(Path::new) {
         let path_str = path.to_str().expect("Convert path");
 
         if let Some(file_name) = path.file_name() {
@@ -224,15 +509,6 @@ fn classify(paths: &[String]) -> Result<ReadPairLookup, Box<dyn Error>> {
             }
         }
     }
-
-    //let mut bad = vec![];
-    //for (key, val) in reads.iter() {
-    //    if !val.contains_key(&ReadDirection::Forward)
-    //        || !val.contains_key(&ReadDirection::Reverse)
-    //    {
-    //        bad.push(key.to_string());
-    //    }
-    //}
 
     let bad: Vec<String> = reads
         .iter()
